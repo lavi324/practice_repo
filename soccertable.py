a6a95882e138cbb70097b04e8bfd57acadd11973
lavi324 <lavialduby@gmail.com>
2023-12-29 00:45:24 +0200

    new

--- soccertable.py
+++ soccertable.py
@@ -12,7 +12,7 @@ teams_data = [
     {"name": "Juventus", "place": 6, "points": 52, "wins": 15, "draws": 7, "losses": 6, "goals_scored": 51, "goals_conceded": 42},
     {"name": "Hapoel tel aviv", "place": 7, "points": 41, "wins": 11, "draws": 8, "losses": 9, "goals_scored": 42, "goals_conceded": 46},
     {"name": "Dortmund", "place": 8, "points": 38, "wins": 10, "draws": 8, "losses": 10, "goals_scored": 41, "goals_conceded": 50},
-    {"name": "Dortmund", "place": 8, "points": 38, "wins": 10, "draws": 8, "losses": 10, "goals_scored": 41, "goals_conceded": 50},
+    {"name": "Sakhnin", "place": 9, "points": 35, "wins": 9, "draws": 8, "losses": 11, "goals_scored": 40, "goals_conceded": 61},
 ]
 
 @app.route('/')
